2f3019d1aa621a2cb177ed2ca0431fab20e82892
linste-zh <linste-zh@gmail.com>
2026-06-27 20:18:15 +0200

    restructuring class responsibilities and DB: - ExperimentItemDAO, ExperimentItemDBO, ExperimentItem separated

--- src/main/java/linstezh/database/dbObjects/ExperimentDBO.java
+++ src/main/java/linstezh/database/dbObjects/ExperimentDBO.java
@@ -1,4 +1,42 @@
 package linstezh.database.dbObjects;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+import linstezh.logic.Experiment;
+
+@DatabaseTable(tableName = "experiments")
 public class ExperimentDBO {
+    @DatabaseField(generatedId = true)
+    private int id;
+
+    @DatabaseField(columnName = "name", canBeNull = false)
+    private String name;
+
+    public ExperimentDBO(){}
+
+    public ExperimentDBO(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public ExperimentDBO(Experiment experiment){
+        this.id = experiment.getID();
+        this.name = experiment.getName();
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/linstezh/logic/Experiment.java
+++ src/main/java/linstezh/logic/Experiment.java
@@ -1,4 +1,46 @@
 package linstezh.logic;
 
+import linstezh.database.dbObjects.ExperimentDBO;
+
+import java.util.List;
+
 public class Experiment {
+    private int id;
+    private String name;
+    private List<SectionInterface> sections;
+
+    public Experiment(int id, String name, List<SectionInterface> sections){
+        this.id = id;
+        this.name = name;
+        this.sections = sections;
+    }
+
+    public Experiment(ExperimentDBO experiment){
+        this.id = experiment.getID();
+        this.name = experiment.getName();
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<SectionInterface> getSections() {
+        return sections;
+    }
+
+    public void setSections(List<SectionInterface> sections) {
+        this.sections = sections;
+    }
 }
