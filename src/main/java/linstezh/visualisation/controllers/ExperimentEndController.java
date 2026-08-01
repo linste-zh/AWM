c5b14bbb7d80d8411e9c44474ecbb45d0509cb11
linste-zh <linste-zh@gmail.com>
2026-08-01 17:01:41 +0200

    switch to Java FXML structure: - Experiment Start basic build applied - Experiment End basic build applied

--- src/main/java/linstezh/visualisation/controllers/ExperimentEndController.java
+++ src/main/java/linstezh/visualisation/controllers/ExperimentEndController.java
@@ -1,13 +1,7 @@
 package linstezh.visualisation.controllers;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import linstezh.executionManagers.EndSectionManager;
 import linstezh.output.resultCSV.CsvAggregatedResultsGenerator;
@@ -18,92 +12,60 @@ import java.io.File;
 import java.io.IOException;
 
 public class ExperimentEndController {
+    @FXML
+    Label infoText;
+
     private InfoItemAdapter item;
     private EndSectionManager manager;
-    VBox box = new VBox();
 
     public void init(InfoItemAdapter item, EndSectionManager manager){
         this.item = item;
         this.manager = manager;
+        infoText.setText(item.readDisplayText());
     }
 
-    public Region createContent() {
-        box.setPadding(new Insets(10, 10, 10, 10));
-        box.setSpacing(5);
+    @FXML
+    private void downloadItemFile() {
+        try {
+            FileChooser fileChooser = new FileChooser();
 
-        box.getChildren().add(header());
-        box.getChildren().add(infoField());
-        box.getChildren().add(downloadItemFile());
-        box.getChildren().add(downloadFAggregatedFile());
-        box.getChildren().add(endExperiment());
+            //Set extension filter for text files
+            FileChooser.ExtensionFilter csvFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
+            fileChooser.getExtensionFilters().add(csvFilter);
 
-        box.setAlignment(Pos.TOP_LEFT);
-        return box;
-    }
+            //Show save file dialog
+            File file = fileChooser.showSaveDialog(manager.getPrimaryStage());
 
-    private Node header() {
-        Label header = new Label("Thank you for participating");
-        header.setFont(new Font(25));
-        header.setAlignment(Pos.CENTER);
-        return header;
-    }
-
-    private Node infoField(){
-        return new Label(this.item.readDisplayText());
-    }
-
-
-    private Node downloadItemFile() {
-        Button results = new Button("Results by Item");
-        results.setOnAction(evt -> {
-            try {
-                FileChooser fileChooser = new FileChooser();
-
-                //Set extension filter for text files
-                FileChooser.ExtensionFilter csvFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
-                fileChooser.getExtensionFilters().add(csvFilter);
-
-                //Show save file dialog
-                File file = fileChooser.showSaveDialog(manager.getPrimaryStage());
-
-                if (file != null) {
-                    manager.requestCsvSave(file, new CsvItemResultsGenerator());
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (file != null) {
+                manager.requestCsvSave(file, new CsvItemResultsGenerator());
             }
-        });
-        return results;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private Node downloadFAggregatedFile() {
-        Button results = new Button("Results by Section");
-        results.setOnAction(evt -> {
-            try {
-                FileChooser fileChooser = new FileChooser();
+    @FXML
+    private void downloadFAggregatedFile() {
+        try {
+            FileChooser fileChooser = new FileChooser();
 
-                //Set extension filter for text files
-                FileChooser.ExtensionFilter csvFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
-                fileChooser.getExtensionFilters().add(csvFilter);
+            //Set extension filter for text files
+            FileChooser.ExtensionFilter csvFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
+            fileChooser.getExtensionFilters().add(csvFilter);
 
-                //Show save file dialog
-                File file = fileChooser.showSaveDialog(manager.getPrimaryStage());
+            //Show save file dialog
+            File file = fileChooser.showSaveDialog(manager.getPrimaryStage());
 
-                if (file != null) {
-                    manager.requestCsvSave(file, new CsvAggregatedResultsGenerator());
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (file != null) {
+                manager.requestCsvSave(file, new CsvAggregatedResultsGenerator());
             }
-        });
-        return results;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private Node endExperiment() {
-        Button results = new Button("End Experiment");
-        results.setOnAction(evt -> {
-            manager.concludeSection();
-        });
-        return results;
+    @FXML
+    private void endExperiment() {
+        manager.concludeSection();
     }
 }
--- src/main/java/linstezh/visualisation/controllers/ExperimentStartController.java
+++ src/main/java/linstezh/visualisation/controllers/ExperimentStartController.java
@@ -1,5 +1,6 @@
 package linstezh.visualisation.controllers;
 
+import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
@@ -11,87 +12,46 @@ import javafx.scene.text.Font;
 import linstezh.executionManagers.StartSectionManager;
 import linstezh.visualisation.adapters.InfoItemAdapter;
 
+import java.util.Objects;
+
 public class ExperimentStartController {
+    @FXML
+    Label title;
+    @FXML
+    Label infoText;
+    @FXML
+    TextField nameInput;
+    @FXML
+    RadioButton consentButton;
+
     private InfoItemAdapter item;
     private StartSectionManager manager;
-    VBox box = new VBox();
 
     public void init(InfoItemAdapter item, StartSectionManager manager){
         this.item = item;
         this.manager = manager;
+
+        title.setText(this.manager.getHeader());
+        infoText.setText(item.readDisplayText());
     }
 
-    public Region createContent() {
-        box.setPadding(new Insets(10, 10, 10, 10));
-        box.setSpacing(5);
-
-        box.getChildren().add(header());
-        box.getChildren().add(infoField());
-        box.getChildren().add(nameInput());
-        box.getChildren().add(consent());
-        box.getChildren().add(start());
-
-        box.setAlignment(Pos.TOP_LEFT);
-        return box;
-    }
-
-    private Node header() {
-        Label header = new Label(this.manager.getHeader());
-        header.setFont(new Font(25));
-        header.setAlignment(Pos.CENTER);
-        return header;
-    }
-
-    private Node nameInput(){
-        Label prompt = new Label("Participant Name or ID: ");
-
-        TextField textField = new TextField();
-        textField.setPromptText("Name/ID");
-        textField.setId("name_input");
-        textField.setPrefWidth(100);
-        textField.setMaxWidth(100);
-        textField.setPrefHeight(3);
-        textField.setMaxHeight(3);
-
-        HBox hbox = new HBox(prompt, textField);
-        hbox.setSpacing(5);
-        return hbox;
-    }
-
-    private Node infoField(){
-        return new Label(this.item.readDisplayText());
-    }
-
-    private Node consent(){
-        RadioButton consentButton =  new RadioButton("I consent to participating in this study");
-        consentButton.setId("consent_button");
-        return consentButton;
-    }
-
-    private Node start() {
-        Button results = new Button("Start Experiment");
-        results.setOnAction(evt -> {
-            RadioButton consentButton = (RadioButton) box.lookup("#consent_button");
-            if(consentButton.isSelected()){
-                TextField textField = (TextField) box.lookup("#name_input");
-                if(textField.getText() != ""){
-                    manager.submitParticipantName(textField.getText());
-                    manager.concludeSection();
-                }else{
-                    Alert a = new Alert(Alert.AlertType.WARNING);
-                    a.setTitle("Missing Name");
-                    a.setContentText("Please provide a participant name or ID.");
-                    a.show();
-                }
+    @FXML
+    private void start() {
+        if(consentButton.isSelected()){
+            if(!Objects.equals(nameInput.getText(), "")){
+                manager.submitParticipantName(nameInput.getText());
+                manager.concludeSection();
             }else{
                 Alert a = new Alert(Alert.AlertType.WARNING);
-                a.setTitle("Consent not given");
-                a.setContentText("Please provide your consent to participate.");
+                a.setTitle("Missing Name");
+                a.setContentText("Please provide a participant name or ID.");
                 a.show();
             }
-
-
-        });
-        return results;
+        }else{
+            Alert a = new Alert(Alert.AlertType.WARNING);
+            a.setTitle("Consent not given");
+            a.setContentText("Please provide your consent to participate.");
+            a.show();
+        }
     }
 }
